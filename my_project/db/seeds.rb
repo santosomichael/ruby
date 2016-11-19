ce2a786b1cd8a733439a8190a8f6a4812493c4bf
Michael Santoso <santosomichael94@gmail.com>
2016-11-19 03:48:01 +0000

    my_project testt

--- my_project/db/seeds.rb
+++ my_project/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Zombie.create(name: "Ash", graveyard: "Glen Haven Memorial")
+Zombie.create(name: "Bob", graveyard: "Chapel Hill Cemetery")
+Zombie.create(name: "Jim", graveyard: "My Father's Basement")
+Tweet.create(status: "Where are you now", zombie_id: "1")
+Tweet.create(status: "Where are you 2", zombie_id: "2")
+Tweet.create(status: "Where are you 3", zombie_id: "3")
+Tweet.create(status: "Where are you 1 lagi", zombie_id: "1")
+
+
+
